8f445dd5504d075744e5ca2af87a03031af011e0
ibratjon <exsaple@gmail.com>
2022-08-11 20:44:55 -0700

    ForEach

---
+++ js/arrey.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const arr = [1, 2, 3, 5, 6, 54,4];
+
+for (const value of arr) {
+    console.log(value);
+} 
---
+++ js/forEach.js
@@ -0,0 +1,8 @@
+'use strict';
+
+
+const arr = [1, 2, 7, 4 ,5];
+
+arr.forEach(function(iteam, index,  arr){
+    console.log(`${index} : ${iteam} into arr ${arr}`);
+})

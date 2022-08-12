2be98facce27a62c8cabc3f6f7727b4db6797c85
ibratjon <exsaple@gmail.com>
2022-08-11 20:02:03 -0700

    theif object qo'shildi

---
+++ js/main2.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const theif = {
+    height: 1.7,
+    jacet:"black",
+    color:{
+        hair:"grey",
+        style:"white",
+    },
+};
+
+console.log(theif);
+
+for (const key in theif) {
+
+    if (typeof theif[key] === "object") {
+        for (let i in theif[key]) {
+        console.log(`Propotaype  ${i}  has value ${theif[key][i]}`);
+           
+        }
+    } else {
+        console.log(`Propotaype  ${key}  has value ${theif[key]}`);
+        
+    }
+}
